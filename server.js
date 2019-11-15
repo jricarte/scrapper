0f8a5d790b6de8c6f8716740dda4be86a0fe6872
Jordan Ricarte <jordan@ricarte.net>
2019-11-14 20:49:17 -0600

    added models folder and modified server to run with mongoose

--- server.js
+++ server.js
@@ -1,127 +1,130 @@
-var mongojs = require("mongojs");
-var cheerio = require("cheerio");
-var axios = require("axios");
 var express = require("express");
-var exphbs = require('express-handlebars');
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
 var app = express();
-var PORT = process.env.PORT || 3000
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
+// Connect to the Mongo DB
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+mongoose.connect(MONGODB_URI);
 
-// Database configuration
-var collections = ["scrapedData"];
+// Routes
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(process.env.MONGODB_URI || 'scraper', collections);
-db.on("error", function (error) {
-  console.log("Database Error:", error);
-});
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("http://www.bioethics.net/news").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-// Main route, render index.handlebars
-app.get("/", function(req, res) {
-  res.render("index");
-})
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("h5").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
 
-// Make a request via axios to grab the HTML body from the site of your choice
-app.get("/scrape", function (req, res) {
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
 
-  db.scrapedData.drop()
-
-  axios
-    .get("https://kotaku.com/")
-    .then(function (response) {
-      // Load the HTML into cheerio and save it to a variable
-      // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-      var $ = cheerio.load(response.data);
-
-      // An empty array to save the data that we'll scrape
-      var results = [];
-
-      // Select each element in the HTML body from which you want information.
-      // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-      // but be sure to visit the package's npm page to see how it works
-      $("article").each(function (i, element) {
-        var title = $(element)
-          .find("h1")
-          .children("a")
-          .text();
-        var link = $(element)
-          .find("h1")
-          .children("a")
-          .attr("href");
-        var image = $(element)
-          .find("source")
-          .attr("data-srcset");
-        var summary = $(element)
-          .find("p").text();
-
-       // If this found element had both a title and a link
-       if (title && link && image && summary) {
-
-        
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link,
-          image: image,
-          summary: summary
-        }, 
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log("scrapedData")
-            console.log(inserted);
-          }
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
         });
-      }
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete!");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // TODO: Finish the route so it grabs all of the articles
+    // Find all results from the scrapedData collection in the db
+    db.Article.find()
+      // Throw any errors to the console
+      .then(function(dbPopulate) {
+        // If any Libraries are found, send them to the client with any associated Books
+        res.json(dbPopulate);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
       });
+});
 
-      // Log the results once you've looped through each of the elements found with cheerio
-      console.log(results);
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "note",
+  // then responds with the article with the note included
+  db.Article.findById(req.params.id)
+  .populate("note")
+  .then(function(dbPopulate) {
+    // If any Libraries are found, send them to the client with any associated Books
+    res.json(dbPopulate);
+  })
+  .catch(function(err) {
+    // If an error occurs, send it back to the client
+    res.json(err);
+  });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+
+  db.Note.create(req.body)
+    .then(function(dbPopulate) {
+      
+      return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: { note: dbPopulate._id } }, { new: true });
     })
+    .then(function(dbPopulate) {
+      // If the Library was updated successfully, send it back to the client
+      res.json(dbPopulate);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
 });
 
-
-// route 1
-app.get("/all", function (req, res) {
-  db.scrapedData.find({}, function (err, found) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.json(found)
-    }
-  });
-});
-
-
-//title route 
-app.get("/title", function(req, res) {
- 
-  db.scrapedData.find().sort({ title: 1 }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.send(found);
-    }
-  });
-});
-
-
-
-
-
-app.listen(PORT, function () {
-  console.log("App running on port 3000!");
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
